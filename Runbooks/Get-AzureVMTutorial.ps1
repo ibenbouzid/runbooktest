792ae59f3c948dfd635051430fae915b5ea824fd
Imad Benbouzid <imad_benb@yahoo.fr>
2017-04-23 03:52:32 +0100

    Update Get-AzureVMTutorial.ps1

--- Runbooks/Get-AzureVMTutorial.ps1
+++ Runbooks/Get-AzureVMTutorial.ps1
@@ -13,7 +13,7 @@ workflow Get-AzureVMTutorial
     $subscriptionName = "INT-Imad-MSFT"
     #Logging in to Azure...
 	# Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name "AzureRunAsConnection"         
+   <# $servicePrincipalConnection=Get-AutomationConnection -Name "AzureRunAsConnection"         
 
     "Logging in to Azure..."
     Add-AzureRmAccount `
@@ -21,26 +21,25 @@ workflow Get-AzureVMTutorial
         -TenantId $servicePrincipalConnection.TenantId `
         -ApplicationId $servicePrincipalConnection.ApplicationId `
         -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-        
-    Select-AzureRmSubscription -SubscriptionName $subscriptionName
-     
+     #>   
+    
     #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
-    #$CredentialAssetName = 'DefaultAzureCredential'
+    $CredentialAssetName = 'DefaultAzureCredential'
 
     #Get the credential with the above name from the Automation Asset store
-    <#$Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
     if(!$Cred) {
         Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
     }
-
+    "Logging in to Azure..."
     #Connect to your Azure Account
     $Account = Add-AzureAccount -Credential $Cred
     if(!$Account) {
         Throw "Could not authenticate to Azure using the credential asset '${CredentialAssetName}'. Make sure the user name and password are correct."
-    }#>
+    }
 
     #TODO (optional): pick the right subscription to use. Without this line, the default subscription for your Azure Account will be used.
-    #Select-AzureSubscription -SubscriptionName "TODO: your Azure subscription name here"
+   Select-AzureRmSubscription -SubscriptionName $subscriptionName
     
     #Get all the VMs you have in your Azure subscription
     $VMs = Get-AzureVM
